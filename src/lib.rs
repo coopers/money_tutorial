6c3b8fe977b107ad27045e92b81b11d2d9efd1e0
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-29 11:59:58 -0800

    Refactor to use f32 instead of i32

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
- use std::num::ParseIntError;
+ use std::num::ParseFloatError;
 
-pub fn parse_money(input: &str) -> Result<(i32, String), ParseIntError> {
+pub fn parse_money(input: &str) -> Result<(f32, String), ParseFloatError> {
     let parts: Vec<&str> = input.split_whitespace().collect();
-    let amount = parts[0].parse()?;
+    let amount: f32 = parts[0].parse()?;
     let currency = parts[1].to_string();
     Ok((amount, currency))
 }
@@ -12,9 +12,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_returns_amount_and_currency_from_a_string() {
+    fn it_returns_amount_and_currency_from_a_string_with_int() {
         if let Ok((amount, currency)) = parse_money("140 Euro"){
-            assert_eq!(amount, 140);
+            assert_eq!(amount, 140.0);
             assert_eq!(currency, "Euro");
         } else {
             assert!(false)
@@ -22,8 +22,18 @@ mod tests {
     }
 
     #[test]
-    fn it_handles_error_when_amount_is_a_float() {
-        let res = parse_money("140.01 Euro");
-        assert!(res.is_err())
+    fn it_returns_amount_and_currency_from_a_string_with_float() {
+        if let Ok((amount, currency)) = parse_money("140.01 Euro"){
+            assert_eq!(amount, 140.01);
+            assert_eq!(currency, "Euro");
+        } else {
+            assert!(false)
+        }
     }
+
+    // #[test]
+    // fn it_handles_error_when_amount_is_a_float() {
+    //     let res = parse_money("140.01 Euro");
+    //     assert!(res.is_err())
+    // }
 }
