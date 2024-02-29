2249c108866ec11d09266e3805a51535ecd0ecfe
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-29 11:52:15 -0800

    handle error when parsing float success

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-pub fn parse_money(input: &str) -> (i32, String) {
+ use std::num::ParseIntError;
+
+pub fn parse_money(input: &str) -> Result<(i32, String), ParseIntError> {
     let parts: Vec<&str> = input.split_whitespace().collect();
-    let amount = parts[0].parse().unwrap();
+    let amount = parts[0].parse()?;
     let currency = parts[1].to_string();
-    (amount, currency)
+    Ok((amount, currency))
 }
 
 #[cfg(test)]
@@ -11,14 +13,17 @@ mod tests {
 
     #[test]
     fn it_returns_amount_and_currency_from_a_string() {
-        let (amount, currency) = parse_money("140 Euro");
-        assert_eq!(amount, 140);
-        assert_eq!(currency, "Euro");
+        if let Ok((amount, currency)) = parse_money("140 Euro"){
+            assert_eq!(amount, 140);
+            assert_eq!(currency, "Euro");
+        } else {
+            assert!(false)
+        }
     }
 
     #[test]
     fn it_handles_error_when_amount_is_a_float() {
-        let (_amount, _currency) = parse_money("140.01 Euro");
-        assert!(true)
+        let res = parse_money("140.01 Euro");
+        assert!(res.is_err())
     }
 }
